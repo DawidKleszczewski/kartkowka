1220ce8e413da935fafce8b1ba9d1aa663ae6235
Dawid Kleszczewski <dawid.kleszczewski9@gmail.com>
2021-02-26 14:34:51 +0100

    end

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
 const arr = new Array(1, 15, 150 ,1500, 'Jan', 5525, 'Henryk')
 
-const wypisanie = arr.map(item =>{
-    console.log(item)
-})
+console.log(arr)
 
 console.log(arr.every(item => item > 1)) 
 
 const indx = arr.findIndex(item => item === 'Jan')
 console.log('Index Jan to' + ' ' + indx)
 
-
-const nowaTab = arr.map(item => item)
-console.log(nowaTab)
+const tab1 = new Array
+const tab2 = new Array
+const tab = arr.map(item => {
+    if(typeof item === 'number'){
+        tab1.push(item)
+    }
+    if(typeof item === 'string'){
+        tab2.push(item)
+    }
+})
+console.log(tab1)
+console.log(tab2)   
